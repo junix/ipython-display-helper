4ac1db02b2d4430b68e0162996d84cac30b357ad
junix <juns1984@qq.com>
2023-02-02 23:34:19 +0800

    fix

--- show.py
+++ show.py
@@ -1,4 +1,6 @@
 
+import itertools
+import pandas as pd
 from dataclasses import dataclass
 from typing import Any
 import inspect
@@ -6,10 +8,114 @@ import types
 
 _attrs = (
     '__name__',
+    '__qualname__',
     '__module__',
+    '__package__',
+    '__loader__',
+    '__spec__',
+    '__file__',
+    '__path__',
     '__class__',
     '__dict__',
+    '__slots__',
     '__sizeof__',
+    '__hash__',
+    '__call__',
+
+    '__format__',
+    '__str__',
+    '__repr__',
+
+
+    '__new__',
+    '__init__',
+    '__del__',
+    '__cantrace__',
+
+
+
+    # math ops
+    '__add__',
+    '__iadd__',
+    '__radd__',
+    '__ladd__',
+    '__sub__',
+    '__isub__',
+    '__rsub__',
+    '__lsub__',
+
+    '__getattribute__',
+    '__getattr__',
+    '__setattr__',
+    '__delattr__',
+
+    # comparison
+    '__lt__',
+    '__le__',
+    '__eq__',
+    '__ne__',
+    '__gt__',
+    '__ge__',
+
+    '__neg__',
+    '__pos__',
+    '__abs__',
+    '__invert__',
+
+    # bitwise ops
+
+    '__lshift__',
+    '__ilshift__',
+    '__rlshift__',
+    '__rshift__',
+    '__irshift__',
+    '__rrshift__',
+    '__xor__',
+    '__ixor__',
+    '__rxor__',
+    '__or__',
+    '__ior__',
+    '__ror__',
+    '__and__',
+    '__iand__',
+    '__rand__'
+    '__invert__'
+    '__round__',
+    '__trunc__',
+    '__floor__',
+    '__ceil__',
+    '__divmod__',
+    '__rdivmod__',
+    '__mod__',
+    '__rmod__',
+    '__imod__',
+    '__div__',
+    '__rdiv__',
+    '__idiv__',
+    '__floordiv__',
+    '__rfloordiv__',
+    '__ifloordiv__',
+    '__mul__',
+    '__rmul__',
+    '__imul__',
+    '__pow__',
+    '__rpow__',
+    '__ipow__',
+    '__matmul__',
+    '__rmatmul__',
+    '__imatmul__',
+    '__truediv__',
+    '__rtruediv__',
+    '__itruediv__',
+
+    # logical ops
+    '__and__',
+    '__or__',
+
+
+
+
+    # functions
     '__code__',
     '__annotations__',
     '__closure__',
@@ -17,26 +123,86 @@ _attrs = (
     '__builtins__',
     '__defaults__',
     '__kwdefaults__',
+    '__self__',
+    '__func__',
+    '__wrapped__',
+    '__iscoroutinefunction__',
+    '__isgeneratorfunction__',
+    '__isabstractmethod__',
+    '__closure_cells__',
 
+    # context managers
+    '__enter__',
+    '__exit__',
+
+    # descriptor
     '__get__',
     '__set__',
     '__delete__',
 
+    # iterators
     '__next__',
     '__iter__',
+    '__getitem__',
+    '__setitem__',
+    '__delitem__',
+    '__missing__',
     '__len__',
-    '__weakref__',
     '__reversed__',
+
     '__contains__',
+    '__weakref__',
+
     '__bool__',
-    '__hash__',
+    '__int__',
+    '__index__',
+    '__float__',
+    '__complex__',
 
     '__match_args__',
 
     '__mro__',
+    '__mro_entries__',
     '__base__',
     '__bases__',
     '__subclasses__',
+    '__init_subclass__',
+    '__instancecheck__',
+    '__subclasscheck__',
+    '__subclasshook__',
+    '__class_getitem__',
+    '__prepare__',
+    '__set_name__',
+
+
+    '__getnewargs__',
+    '__getnewargs_ex__',
+    '__getinitargs__',
+    '__getstate__',
+    '__setstate__',
+    '__reduce__',
+    '__reduce_ex__',
+    '__copy__',
+    '__deepcopy__',
+    '__dir__',
+    '__doc__',
+    '__text_signature__',
+
+
+    # coroutines
+    '__await__',
+    '__anext__',
+    '__aiter__',
+    '__aenter__',
+    '__aexit__',
+
+    # exceptions
+    '__context__',
+    '__cause__',
+    '__suppress_context__',
+    '__traceback__',
+    '__traceback_hide__',
+
 
     'co_argcount',
     'co_cellvars',
@@ -62,8 +228,10 @@ _attrs = (
 @dataclass
 class Attr:
     name: str
-    type: str | None | inspect.Signature
+    type: str | None
+    sig: str | None | inspect.Signature
     value: Any
+    overwrite: str = ''
 
     # def __str__(self) -> str:
     #     return f"{self.name:<20} {self.type:<20} {self.value}"
@@ -73,55 +241,131 @@ def type_name(obj):
     return type(obj).__name__
 
 
-def is_no_arg_callable(x) -> bool:
+def get_signature(call):
     try:
-        return len(inspect.signature(x).parameters) == 0
+        sig = inspect.signature(call)
+        if sig.return_annotation is inspect.Signature.empty:
+            sig = sig.replace(return_annotation='▧')
+        return str(sig).replace("'", '')
+    except:
+        return type(call).__name__
+
+
+def is_no_arg_callable(x) -> bool:
+    if not callable(x) or inspect.isbuiltin(x):
+        return False
+
+    try:
+        sig = inspect.signature(x)
+        return not sig.parameters
     except:
         return False
+
+
+def _uniq(iterable):
+    seen = set()
+    for x in iterable:
+        if x not in seen:
+            yield x
+            seen.add(x)
+
+
+def _attr_type(v):
+    attr_type = type(v).__name__
+    if inspect.isfunction(v):
+        attr_type = 'function'
+    elif inspect.ismethod(v):
+        attr_type = 'method'
+    elif inspect.isgetsetdescriptor(v):
+        attr_type = 'getsetdescriptor'
+    elif inspect.ismemberdescriptor(v):
+        attr_type = 'memberdescriptor'
+    elif inspect.isdatadescriptor(v):
+        attr_type = 'datadescriptor'
+    elif inspect.isgenerator(v):
+        attr_type = 'generator'
+
+    return attr_type
+
+def is_overwrite(f):
+    if not callable(f):
+        return False
+    match type(f).__name__:
+        case 'method-wrapper':
+            self = f.__self__
+            class_func = getattr(type(self), f.__name__,None)
+            return class_func is not getattr(object, f.__name__,None)
+        case 'method'|"function":
+            return True
+        case 'builtin_function_or_method':
+            return False
+        case "wrapper_descriptor":
+            return f is not getattr(object, f.__name__,None)
+        case _:
+            print(">>>>", type(f))
+    return False
+
+def is_dunder(attr):
+    return attr.startswith('__') and attr.endswith('__')
+
 
 def get_attr_info(obj, attr, *, skip_none=True):
     from dataclasses import replace
     if skip_none and not hasattr(obj, attr):
         return
 
-    attr_v = getattr(obj, attr, None)
-    attr_type = type(attr_v).__name__
-    spec = Attr(attr, attr_type, attr_v)
-    match (attr, attr_v):
+    v = getattr(obj, attr, None)
+    spec = Attr(name=attr, type=_attr_type(v), value=v, sig='')
+
+    if is_dunder(attr) and is_overwrite(v):
+        spec = replace(spec, overwrite='▧')
+
+    if obj.__class__ is type and inspect.ismethod(v):
+        spec = replace(spec, type=spec.type+"[staticmethod]")
+
+    match(attr, v):
         case _, None:
             if not skip_none:
                 yield replace(spec, value='-', type='-')
+
+        case '__lt__' | '__le__' | '__eq__' | '__ne__' | '__gt__' | '__ge__', func:
+            # if is overwrite, skip
+            match type(v).__name__:
+                case 'method' | 'function':
+                    yield replace(spec, sig=get_signature(func))
+                case _:
+                    return
+
         case '__closure__', closure:
             yield replace(spec, value=[c.cell_contents for c in closure])
-        case '__class__', cls:
-            yield replace(spec, value=cls.__name__)
-        case '__mro__'|'__bases__', classes:
+        case '__class__', _:
+            yield replace(spec, value=v.__name__)
+        case '__subclasses__', classes:
+            yield replace(spec, value=tuple(c.__name__ for c in classes()))
+        case '__mro__' | '__bases__' | '__subclasses__', classes:
             yield replace(spec, value=tuple(c.__name__ for c in classes))
-        case [_, func] if is_no_arg_callable(func):
+        case '__dir__'|'__doc__', dirs:
+            yield replace(spec, value='')
+        case '__init__', '__call__', call:
+            yield replace(spec, sig=get_signature(call), value='')
+        case[_, func] if is_dunder(attr) and is_no_arg_callable(func):
             result = func()
-            yield replace(spec, value=result, type=f'()->{type_name(result)}')
+            yield replace(spec, value=result, sig=f'()->{type_name(result)}')
         case '__globals__', globals:
             yield replace(spec, value=f"{len(globals)} vars")
         case '__dict__', attr_dict:
-            yield replace(spec, value=attr_v.keys())
+            yield replace(spec, value=v.keys())
         case '__code__', code:
             yield replace(spec, value=code.co_name)
-        case _, str(v) | int(v) | float(v) | bool(v)|bytes(v):
+        case _, str(v) | int(v) | float(v) | bool(v) | bytes(v):
             yield spec
-        case _, dict()|types.MappingProxyType():
-            if len(attr_v) < 10:
+        case _, dict() | types.MappingProxyType():
+            if len(v) < 10:
                 yield spec
             else:
-                yield replace(spec, value=f"{len(attr_v)} vars")
-        case [_, call] if callable(call):
-            try:
-                sig = inspect.signature(call)
-                if sig.return_annotation is inspect.Signature.empty:
-                    sig = sig.replace(return_annotation='▧')
-                yield replace(spec, type=str(sig))
-            except e:
-                print(e)
-                yield spec
+                yield replace(spec, value=f"{len(v)} vars")
+        case[_, call] if callable(call):
+            yield replace(spec, sig=get_signature(call))
         case _:
             yield spec
 
@@ -131,14 +375,76 @@ def fetch_attrs(obj, *, attrs=_attrs, **kwargs):
         yield from get_attr_info(obj, attr, **kwargs)
 
 
-import pandas as pd
-
-
 def tabulate_attrs(attrs: list[Attr]) -> pd.DataFrame:
     return pd.DataFrame(
         {
             'type': [x.type for x in attrs],
-            'value': [x.value for x in attrs]
+            'sig': [x.sig for x in attrs],
+            'value': [x.value for x in attrs],
+            'overwrite': [x.overwrite for x in attrs],
         },
         index=[x.name for x in attrs],
     )
+
+
+def desc(obj, *, attrs=_attrs, include_all: bool = False, **kwargs):
+    if include_all:
+        attrs = list(_uniq(itertools.chain(attrs, [attr for attr, _ in inspect.getmembers(obj)])))
+    return tabulate_attrs(tuple(fetch_attrs(obj, attrs=attrs, **kwargs)))
+
+
+def compare_attrs(xs, *, attrs=None, hidden_common=True):
+    if isinstance(xs, list | tuple):
+        xs = {str(x): x for x in xs}
+
+    attr_set = {attr for x in xs.values() for attr,_ in inspect.getmembers(x)}
+    for x in attr_set:
+        assert x in _attrs, f"{x} not in _attrs, please add it to _attrs"
+
+    attrs = [x for x in _attrs if x in attr_set]
+    attrs = pd.DataFrame(
+        data={
+            k: ['√' if hasattr(v, attr) else 'ㄨ' for attr in attrs]
+            for k, v in xs.items()
+        },
+        index=attrs
+    )
+
+    if hidden_common and len(attrs) > 1:
+        h, *ts = attrs
+        print('>>>>>',ts)
+        index, *indices = (attrs[t] == attrs[h] for t in ts)
+        for x in indices:
+            index &= x
+        attrs = attrs[~index]
+
+    return attrs
+
+
+def block_display(df: pd.DataFrame):
+    import IPython
+    from IPython.display import display, HTML
+    from itertools import pairwise
+    n = 0
+    while n <= df.shape[0]:
+        display(HTML(df.iloc[n:n+15].to_html()))
+        n += 15
+
+
+def h(o):
+    from docutils.core import publish_string
+    from IPython.display import display_html,HTML
+
+    import contextlib
+    import io
+    
+    doc = getattr(o, '__doc__',None)
+    if not doc:
+        help(o)
+        return
+
+    with contextlib.redirect_stderr(io.StringIO()):
+        try:
+            display_html(HTML(publish_string(doc, writer_name="html").decode('utf-8')))
+        except:
+            help(o)
---
+++ wanglijun.py
@@ -0,0 +1,75 @@
+def ipython_width(width: int):
+    from IPython.display import display, HTML
+    display(
+        HTML(f"<style>.container {{ width:{width}% !important; }}</style>"))
+
+
+def ipython_display_max_rows(rows: int | None = None):
+    import pandas as pd
+    pd.set_option('display.max_rows', rows)
+
+
+"""Check if the code is running in Jupyter."""
+try:
+    match get_ipython().__class__.__name__:
+        case "ZMQInteractiveShell":
+            _style = 'default'
+            _output_fmt = "html"
+        case _:
+            _style = 'monokai'
+            _output_fmt = "terminal"
+except NameError:
+    _output_fmt = "terminal"
+
+
+def highlight_source(
+    code: str,
+    *,
+    style: str = _style,
+    language: str = 'python',
+    output_fmt: str = _output_fmt,
+) -> str:
+    """Highlight source code."""
+    from pygments import lexers, highlight
+    from pygments.styles import get_style_by_name, get_all_styles
+    lexer = lexers.get_lexer_by_name(language)
+    style = get_style_by_name(style)
+    match output_fmt:
+        case "html":
+            from pygments.formatters.html import HtmlFormatter
+            formatter = HtmlFormatter(full=True, style=style)
+        case "terminal" | "term" | "terminal256":
+            from pygments.formatters.terminal256 import Terminal256Formatter
+            formatter = Terminal256Formatter(style=style)
+        case _:
+            raise ValueError(
+                f"output_fmt must be 'html' or 'terminal', but got {output_fmt}.")
+    return highlight(code, lexer, formatter)
+
+
+def show_source(
+    obj,
+    *,
+    style: str = _style,
+    language: str = 'python',
+    output_fmt: str = _output_fmt
+):
+    """Show source code of an object."""
+    import inspect
+    if inspect.isbuiltin(obj):
+        print("builtin, no source code available")
+        return
+
+    code = inspect.getsource(obj)
+    highlight_code = highlight_source(
+        code, style=style, language=language, output_fmt=output_fmt)
+    # Highlight
+    match output_fmt:
+        case "html":
+            from IPython.display import display_html, HTML
+            display_html(HTML(highlight_code))
+        case "terminal" | "term" | "terminal256":
+            print(highlight_code)
+        case _:
+            raise ValueError(
+                f"output_fmt must be 'html' or 'terminal', but got {output_fmt}.")
